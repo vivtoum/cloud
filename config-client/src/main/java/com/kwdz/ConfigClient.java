77fb6651437e874e854b0f90d43943791204bbb9
vivtoum <vedyou@qq.com>
2019-03-25 03:02:33 +0800

    修改：config-git服务端，读取github的配置

--- config-client/src/main/java/com/kwdz/ConfigClient.java
+++ config-client/src/main/java/com/kwdz/ConfigClient.java
@@ -1,15 +1,35 @@
 package com.kwdz;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
+@RefreshScope
 public class ConfigClient {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConfigClient.class).web(true).run(args);
+    }
+
+    @Value("${spring.application.name}")
+    private String projectName;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${test}")
+    private String test;
+
+    @RequestMapping("/")
+    public String from() {
+        return this.projectName + ":" + this.port + "   and test:" + test;
     }
 
 }
