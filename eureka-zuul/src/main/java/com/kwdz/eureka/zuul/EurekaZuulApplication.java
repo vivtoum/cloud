7ae9044e728325744de4a1a15ca13660d7eddf47
vivtoum <vedyou@qq.com>
2019-04-18 00:31:14 +0800

    构建脚手架：接入oauth2实现登录

--- eureka-zuul/src/main/java/com/kwdz/eureka/zuul/EurekaZuulApplication.java
+++ eureka-zuul/src/main/java/com/kwdz/eureka/zuul/EurekaZuulApplication.java
@@ -3,13 +3,17 @@ package com.kwdz.eureka.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy //开启网关Zuul
 @EnableEurekaClient
 @EnableSwagger2
 @SpringBootApplication
+@ComponentScan({"com.kwdz.*"})
+@EnableFeignClients({"com.kwdz.*"})
 public class EurekaZuulApplication {
 
     public static void main(String[] args) {
--- eureka-zuul/src/main/java/com/kwdz/eureka/zuul/filter/AccessFilter.java
+++ eureka-zuul/src/main/java/com/kwdz/eureka/zuul/filter/AccessFilter.java
@@ -1,19 +1,32 @@
 package com.kwdz.eureka.zuul.filter;
 
+import com.kwdz.eureka.zuul.oauth2.SsoFeign;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.util.ZuulRuntimeException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
 /**
  * 服务网关过滤器
  */
 @Component
 public class AccessFilter extends ZuulFilter {
+
+    @Autowired
+    private SsoFeign ssoFeign;
+
 
     /**
      * 返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型：
@@ -26,14 +39,22 @@ public class AccessFilter extends ZuulFilter {
      */
     @Override
     public String filterType() {
-        return "pre";
+        // PreDecoration之前运行
+        return PRE_TYPE;
     }
 
+    /**
+     * 通过int值来定义过滤器的执行顺序
+     */
     @Override
     public int filterOrder() {
-        return 0;
+        return PRE_DECORATION_FILTER_ORDER - 1;
     }
 
+
+    /**
+     * 返回一个boolean类型来判断该过滤器是否要执行
+     */
     @Override
     public boolean shouldFilter() {
         RequestContext ctx = RequestContext.getCurrentContext();
@@ -49,20 +70,38 @@ public class AccessFilter extends ZuulFilter {
      */
     @Override
     public Object run() throws ZuulRuntimeException {
+
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
-        System.out.println(String.format("%s demoFilter request to %s", request.getMethod(), request.getRequestURL().toString()));
-        String username = request.getParameter("username");// 获取请求的参数
-        if (!StringUtils.isEmpty(username) && username.equals("bright")) {//当请求参数username为“bright”时通过
-            ctx.setSendZuulResponse(true);// 对该请求进行路由
+        HttpServletResponse response = ctx.getResponse();
+
+        //访问路径
+        String url = request.getRequestURL().toString();
+
+        //从cookie里面取值（Zuul丢失Cookie的解决方案：https://blog.csdn.net/lindan1984/article/details/79308396）
+        String accessToken = request.getParameter("accessToken");
+        Cookie[] cookies = request.getCookies();
+        if (null != cookies) {
+            for (Cookie cookie : cookies) {
+                if ("accessToken".equals(cookie.getName())) {
+                    accessToken = cookie.getValue();
+                }
+            }
+        }
+        //过滤规则：cookie有令牌且存在于Redis，或者访问的是登录页面、登录请求则放行
+        if (url.contains("sso-server/sso/loginPage") || url.contains("sso-server/sso/login") || (!StringUtils.isEmpty(accessToken) && ssoFeign.hasKey(accessToken))) {
+            ctx.setSendZuulResponse(true);
             ctx.setResponseStatusCode(200);
-            ctx.set("isSuccess", true);// 设值，让下一个Filter看到上一个Filter的状态
             return null;
         } else {
-            ctx.setSendZuulResponse(false);// 过滤该请求，不对其进行路由
-            ctx.setResponseStatusCode(401);// 返回错误码
-            ctx.setResponseBody("{\"result\":\"username is not correct!\"}");// 返回错误内容
-            ctx.set("isSuccess", false);
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            //重定向到登录页面
+            try {
+                response.sendRedirect("//http://118.24.203.80:8001/sso-server/sso/loginPage?url=" + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return null;
         }
     }
