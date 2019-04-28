6308dce79a7c173d9f60db8fe9919eb649a835f0
vivtoum <vedyou@qq.com>
2019-04-28 17:42:56 +0800

    添加：单点登录

--- eureka-zuul/src/main/java/com/kwdz/eureka/zuul/EurekaZuulApplication.java
+++ eureka-zuul/src/main/java/com/kwdz/eureka/zuul/EurekaZuulApplication.java
@@ -1,23 +1,33 @@
 package com.kwdz.eureka.zuul;
 
+import com.kwdz.eureka.zuul.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy //开启网关Zuul
 @EnableEurekaClient
 @EnableSwagger2
+@EnableOAuth2Sso
 @SpringBootApplication
-@ComponentScan({"com.kwdz.*"})
-@EnableFeignClients({"com.kwdz.*"})
-public class EurekaZuulApplication {
+public class EurekaZuulApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaZuulApplication.class, args);
     }
 
+    /**
+     * 资源过滤器
+     *
+     * @return 资源过滤器
+     */
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 }
