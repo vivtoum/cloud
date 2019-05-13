8098caa71f4518b7ad5d230b3fae1fa62ca3637d
vivtoum <vedyou@qq.com>
2019-05-13 21:00:06 +0800

    Merge branch 'master' of D:\idea-project\cloud with conflicts.

--- eureka-zuul/src/main/java/com/kwdz/eureka/zuul/EurekaZuulApplication.java
+++ eureka-zuul/src/main/java/com/kwdz/eureka/zuul/EurekaZuulApplication.java
@@ -1,13 +1,12 @@
 package com.kwdz.eureka.zuul;
 
-import com.kwdz.eureka.zuul.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy //开启网关Zuul
@@ -15,19 +14,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableOAuth2Sso
 @SpringBootApplication
-public class EurekaZuulApplication extends WebSecurityConfigurerAdapter {
+public class EurekaZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaZuulApplication.class, args);
     }
 
-    /**
-     * 资源过滤器
-     *
-     * @return 资源过滤器
-     */
-    @Bean
-    public AccessFilter accessFilter() {
-        return new AccessFilter();
-    }
 }
--- sso-server/src/main/java/com/kwdz/sso/server/SsoServerApplication.java
+++ sso-server/src/main/java/com/kwdz/sso/server/SsoServerApplication.java
@@ -2,14 +2,13 @@ package com.kwdz.sso.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableOAuth2Sso
-public class SsoServerApplication extends WebSecurityConfigurerAdapter {
+public class SsoServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SsoServerApplication.class, args);
